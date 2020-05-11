8ec42bc43d93cf2b443d0f7151d3538f3b8ae21e
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-11 01:50:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,8 @@
-array = [1,2,3]
-def square_array(array)
-  array.each do |num|
-  array[num += 1] ** 2
+numbers = [1,2,3]
+def square_array(numbers)
+  array.new = []
+  numbers.each do |num|
+  num ** 2
+  array << num 
   end
 end
